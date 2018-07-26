27d8e1298aae9f4d98101151ee8d1866b9b67e74
Philip Diffenderfer <pdiffenderfer@gmail.com>
2018-07-26 11:58:56 -0400

    Removing files not necessary for gh-pages

--- src/lib.js
+++
@@ -1,9 +0,0 @@
-
-import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
-import DaySpanVuetify from './plugin'
-
-export * from './components';
-
-export default DaySpanVuetify;
