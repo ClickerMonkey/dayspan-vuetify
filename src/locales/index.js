90f932e5409738d20f87da76cf0ab7076c106c64
ibbtek <25365069+ibbtek@users.noreply.github.com>
2018-11-20 20:04:23 +0100

    Update index.js
    add missing import to existing french locale file in index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,9 +1,13 @@
 
 import en from './en'
+import fr from './fr'
 
 export default {
   'en': en,
-  'en-US': en
+  'en-US': en,
+  'fr': fr,
+  'fr-BE': fr,
+  'fr-FR': fr
 }
 
 export const defaultLocale = 'en'
